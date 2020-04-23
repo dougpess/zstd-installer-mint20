ed62dfb7b9ce1065cefec2a3310b5a49642eafb2
dougpess <dougpess@hotmail.com>
2020-04-23 18:47:46 -0300

    Create zstd-installer.sh

---
+++ zstd-installer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt update -qq
+apt install -y checkinstall build-essential
+cd /tmp
+wget https://github.com/facebook/zstd/releases/download/v1.4.4/zstd-1.4.4.tar.gz -O zstd.tar.gz
+tar -xf zstd.tar.gz
+cd zstd-1.4.4
+make
+echo y | checkinstall
+if ! grep -qx zstd /etc/initramfs-tools/modules; then echo zstd >> /etc/initramfs-tools/modules; fi
+if ! grep -qx zstd_compress /etc/initramfs-tools/modules; then echo zstd_compress >> /etc/initramfs-tools/modules; fi
+if ! grep -qx z3fold /etc/initramfs-tools/modules; then echo z3fold >> /etc/initramfs-tools/modules; fi
+update-initramfs -u -k all
